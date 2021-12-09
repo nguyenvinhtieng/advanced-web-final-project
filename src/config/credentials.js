782c5f82969720cfde33ae0bcb0743898b35cd7f
vinhtieng <vinhtieng123@gmail.com>
2021-12-09 13:59:30 +0700

    Fix

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -4,9 +4,11 @@ module.exports = {
         "connectionString": "mongodb+srv://vinhtieng:vinhtieng@cluster0.ugswm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     },
     "google": {
-        "clientID": "459825136805-c1lbq1urssbu0risuki18c6uoja51o6n.apps.googleusercontent.com",
-        "clientSecret": "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",
-        "callbackURL": "http://localhost:3000/google/callback"
+        "clientID": "604767619179-tsd16h2s3j7guebeje4jorjfere0nddd.apps.googleusercontent.com",
+        "clientSecret": "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",
+        "callbackURL": "https://advanced-web-tdtu-2021-2022.herokuapp.com/google/callback"
     }
 }
+//459825136805-c1lbq1urssbu0risuki18c6uoja51o6n.apps.googleusercontent.com
+//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
 // mongodb+srv://vinhtieng:<password>@cluster0.ugswm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
