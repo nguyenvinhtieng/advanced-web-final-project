253f79ee093470fc16a295c9b163167991fd0300
vinhtieng <vinhtieng123@gmail.com>
2021-12-09 14:01:24 +0700

    fix

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,7 +1,7 @@
 module.exports = {
     "cookieSecret": "kghkasfcgnasuyg6526as",
     "mongo": {
-        "connectionString": "mongodb+srv://vinhtieng:vinhtieng@cluster0.ugswm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        "connectionString": "mongodb+srv://vinhtieng:vinhtieng@cluster0.ugswm.mongodb.net/advanced-web-21-22?retryWrites=true&w=majority"
     },
     "google": {
         "clientID": "604767619179-tsd16h2s3j7guebeje4jorjfere0nddd.apps.googleusercontent.com",
