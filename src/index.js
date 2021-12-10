5fa583164d29c8a2f840990284d681e6ceacbfd8
Dung <vandung.alpha@gmail.com>
2021-12-10 09:27:58 +0700

    Delete package-lock.json

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const credentials = require('./config/credentials');
 const route = require('./route/index');
